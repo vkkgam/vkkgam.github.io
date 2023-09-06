c4273d35a908626713a0acdee3c63fb664f68412
vkkgam <128412528+vkkgam@users.noreply.github.com>
2023-09-06 13:29:26 +0300

    Add files via upload

---
+++ ovoch/scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+
+
+
+const scriptsInEvents = {
+
+	async Gameevent_Event2_Act1(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#7bb93f'
+	},
+
+	async Menuevent_Event1_Act3(runtime, localVars)
+	{
+		document.querySelector('body').style['backgroundColor'] = '#7bb93f'
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
